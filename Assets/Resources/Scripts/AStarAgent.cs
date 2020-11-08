dfe467021425df93077cb9648e2d753af65c93cc
ReedhamHackerman <48519104+ReedhamHackerman@users.noreply.github.com>
2020-11-08 18:45:21 -0500

    AIAgent
    SecondLastCommite

---
+++ Assets/Resources/Scripts/AStarAgent.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AStarAgent : MonoBehaviour
+{
+    MainFlow mainFlow;
+    Node currentNode;
+    Dictionary<Vector2Int, Node> openNodes = new Dictionary<Vector2Int, Node>();
+    Dictionary<Vector2Int, Node> closeNodes = new Dictionary<Vector2Int, Node>();
+    public void Initialize(MainFlow mainFlow)
+    {
+        this.mainFlow = mainFlow;
+    }
+
+    public void CalculatePath()
+    {
+        currentNode = new Node(mainFlow.startAIPos, Heuristic(mainFlow.startAIPos), 0, null);
+    }
+
+
+    public float Heuristic(Vector2Int nodePos)
+    {
+        return Vector2.Distance(nodePos, mainFlow.player.gameObject.transform.position);
+    }
+
+}
+public class Node
+{
+    public Vector2Int NodePos { get; private set; }
+    public float H { get; private set; }
+    public float G { get; private set; }
+    public float F { get; private set; }
+    public Node Parent { get; private set; }
+
+
+    public Node(Vector2Int nodePos,float h,float g,Node parent)
+    {
+        NodePos = nodePos;
+        G = g;
+        H = h;
+        F = g + h;
+        Parent = parent;
+    }
+
+
+    public void ChangeGAndFAndParent(float newG,Node newParent)
+    {
+        G = newG;
+        F = G + H;
+        Parent = newParent;
+    }
+
+}
---
+++ Assets/Resources/Scripts/MainFlow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainFlow : MonoBehaviour
+{
+    public GridColumn[] gameBoard;
+    public Vector2Int startAIPos ;
+    public bool[,] Walls { get; private set; } = new bool[10, 10]; //false are open
+    AStarAgent AI;
+    public GameObject player;
+    void Start()
+    {
+        FillWallArray();
+        SpawnPieces();
+    }
+
+    
+    void Update()
+    {
+        AI.CalculatePath();
+    }
+    void FillWallArray()
+    {
+        for(int i = 0;i<gameBoard.Length;i++)
+        {
+            for (int j = 0; j < gameBoard[i].squares.Length; j++)
+            {
+                if(!gameBoard[i].squares[j].activeSelf)
+                {
+                    Walls[i, j] = true;
+                }
+            }
+        }
+    }
+    void SpawnPieces()
+    {
+      AI =   Instantiate<AStarAgent>(Resources.Load<AStarAgent>("Prefabs/AI"), new Vector3(startAIPos.x,startAIPos.y,0), Quaternion.identity);
+      AI.Initialize(this);
+    }
+}
